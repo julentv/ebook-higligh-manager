855bda8a8cf469c747332bef9c5b3aaeaac07a9b
Julen <julentv@gmail.com>
2020-09-27 16:41:16 +0200

    CervantesBookProvider finnished

--- cervantes-book-data-extractor/src/main/java/julentv/books/cervantes/CervantesBookProvider.java
+++ cervantes-book-data-extractor/src/main/java/julentv/books/cervantes/CervantesBookProvider.java
@@ -1,23 +1,35 @@
 package julentv.books.cervantes;
 
 import julentv.books.Book;
+import julentv.books.BookMetadata;
 import julentv.books.BookProvider;
-import julentv.books.cervantes.extraction.CervantesXmlBookExtractor;
+import julentv.books.highlight.Highlight;
 import julentv.books.highlight.extraction.cervantes.CervantesXmlHighlightExtractor;
 
 import java.io.File;
+import java.util.List;
 
 public class CervantesBookProvider implements BookProvider {
-    private final CervantesXmlBookExtractor bookExtractor;
+    private final Books books;
     private final CervantesXmlHighlightExtractor highlightExtractor;
 
-    public CervantesBookProvider(CervantesXmlBookExtractor bookExtractor, CervantesXmlHighlightExtractor highlightExtractor) {
-        this.bookExtractor = bookExtractor;
+    public CervantesBookProvider(Books books, CervantesXmlHighlightExtractor highlightExtractor) {
+        this.books = books;
         this.highlightExtractor = highlightExtractor;
     }
 
     @Override
-    public Book getBook(File file) {
-        return null;
+    public Book getBook(File notesFile) {
+        List<Highlight> highlights = highlightExtractor.getHighlights(notesFile);
+        BookMetadata metadata = getCervantesBookMetadata(notesFile);
+        return new Book(metadata, highlights);
+    }
+
+    private BookMetadata getCervantesBookMetadata(File notesFile) {
+        julentv.books.cervantes.Book cervantesMetadata = books.getBooks().stream()
+                .filter(book -> book.getFileName().equals(notesFile.getName()))
+                .findFirst()
+                .orElseThrow();
+        return new BookMetadata(cervantesMetadata.getTitle(), cervantesMetadata.getAuthor());
     }
 }
--- cervantes-book-data-extractor/src/test/java/julentv/books/cervantes/CervantesBookProviderTest.java
+++ cervantes-book-data-extractor/src/test/java/julentv/books/cervantes/CervantesBookProviderTest.java
@@ -1,7 +1,7 @@
 package julentv.books.cervantes;
 
 import julentv.books.Book;
-import julentv.books.cervantes.extraction.CervantesXmlBookExtractor;
+import julentv.books.highlight.Highlight;
 import julentv.books.highlight.extraction.cervantes.CervantesXmlHighlightExtractor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,26 +10,44 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 class CervantesBookProviderTest {
     @Mock
-    private CervantesXmlBookExtractor bookExtractor;
-    @Mock
     private CervantesXmlHighlightExtractor highlightExtractor;
     @Mock
-    private File mockFile;
+    private File highlightFile;
+    @Mock
+    private List<Highlight> highlights;
     private CervantesBookProvider provider;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        provider = new CervantesBookProvider(bookExtractor, highlightExtractor);
+        Books books = new Books();
+        books.setBooks(Arrays.asList(createBook("book1", "path/book1.html", "author1"), createBook("book2", "path/book2.html", "author2")));
+        provider = new CervantesBookProvider(books, highlightExtractor);
     }
 
     @Test
     void getBook() {
-        Book book = provider.getBook(mockFile);
-        Assertions.assertEquals(book.getMetadata().getTitle(), "test Title");
-        Assertions.assertEquals(book.getMetadata().getAuthor(), "test Author");
+        when(highlightExtractor.getHighlights(highlightFile)).thenReturn(highlights);
+        when(highlightFile.getName()).thenReturn("book1.html");
+
+        Book book = provider.getBook(highlightFile);
+        Assertions.assertEquals(book.getMetadata().getTitle(), "book1");
+        Assertions.assertEquals(book.getMetadata().getAuthor(), "author1");
+        Assertions.assertEquals(book.getHighlights(), highlights);
+    }
+
+    private julentv.books.cervantes.Book createBook(String title, String path, String author) {
+        var book = new julentv.books.cervantes.Book();
+        book.setPath(path);
+        book.setTitle(title);
+        book.setAuthor(author);
+        return book;
     }
 }
